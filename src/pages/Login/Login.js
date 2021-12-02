94bc4b0f25420878e4fea36cd3bea52146d520fd
aldrisvilla <aldrisv@uninorte.edu.co>
2021-12-02 14:18:53 -0500

    login + cambios

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
-import React,{useState} from "react";
-import "../Login/Login.css"
+import React from "react";
+import "../Login/Login.css";
 import Title from './Components/Title/Title';
 import Label from "./Components/Label/Label"
 import Input  from "./Components/Input/Input";
@@ -8,34 +8,38 @@ import Input  from "./Components/Input/Input";
 
 const Login=()=>{
 
-    const[user,setUser]=useState("");
-    const[password,setPassword]=useState("");
+    // const[user,setUser]=useState("");
+    // const[password,setPassword]=useState("");
 
-    function handleChange(name,value){
-        if(name==="usuario"){
-            setUser(value)
+    // function handleChange(name,value){
+    //     if(name==="usuario"){
+    //         setUser(value)
 
-        }else{
-            if(name==="password"){
-                setPassword(value)
-            }
-        }
-    };
-    console.log("usuario: ",user)
-    console.log("password:",password)
+    //     }else{
+    //         if(name==="password"){
+    //             setPassword(value)
+    //         }
+    //     }
+    // };
+    // console.log("usuario: ",user)
+    // console.log("password:",password)
 
-    function hanledSubmit(){
-        let account={user,password}
-        if(account){
-            console.log("account: ",account)
-        }
+    // function hanledSubmit(){
+    //     let account={user,password}
+    //     if(account){
+    //         console.log("account: ",account)
+    //     }
 
-    }
+    // }
     return(
         <div className="container-login ">
             <div className="sub-container-login">
-            <h1>Quick Delivey</h1>
-            <Label text="Login"></Label>
+            <h1>Quick Delivery</h1>
+            
+            <hr/>
+
+
+            <strong><Title text="Login" className="login-titulo"></Title></strong>
             <Label text="Usuario"></Label>
             <Input 
             attribute={{
@@ -44,7 +48,7 @@ const Login=()=>{
                 type:"text",
                 placeholder:"Digite su usuario"
             }}
-            handleChange={handleChange} 
+            // handleChange={handleChange} 
             />
             <Label text="Contraseña"></Label>
             <Input 
@@ -56,14 +60,30 @@ const Login=()=>{
 
 
             }}
-            handleChange={handleChange}
+            // handleChange={handleChange}
             />
 
+            <br/>
+
             <button className="btn btn-primary" 
-            onClick={hanledSubmit} 
-            >ingresar
+            
+            >Ingresar
             
             </button>
+
+            
+
+            <button className="btn btn-primary" 
+             
+            >Registrarse
+            
+            </button>
+            <br/>
+            <br/>
+            <br/>
+
+            <p><small>Diseñado por, GP2 Systems © 2021</small></p>
+
 
             
 
